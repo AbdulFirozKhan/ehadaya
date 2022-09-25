1cf4ff0141bb58fb27d4c7028eb587a46c2b0029
LAPTOP-RD6E64U9\hp <khan.abdul.firoz@gmail.com>
2022-09-25 22:05:15 +0400

    hotel and car renatal pages added

--- src/components/travel/car-rental.js
+++ src/components/travel/car-rental.js
@@ -1,11 +1,96 @@
-import React, { Component } from 'react'
-import './car-rental.css'
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import InputLabel from "@mui/material/InputLabel";
+import "./car-rental.css";
 
-export default class HotelsComponent extends Component { 
+export default function CarRentalComponent() {
+  return (
+    <Stack spacing={2}>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Stack spacing={0}>
+            <InputLabel>Counntry</InputLabel>
+            <Select
+              labelId="Select Counntry"
+              id="demo-simple-select"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            ></Select>
+          </Stack>
+          <Stack spacing={0}>
+            <InputLabel>City</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="Select City"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            >
+              <MenuItem value={10}></MenuItem>
+            </Select>
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Stack spacing={0}>
+          <InputLabel>Location</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="Select Location"
+            onChange=""
+            sx={{
+              minWidth: "520px",
+              height: "40px",
+            }}
+          >
+            <MenuItem value={10}></MenuItem>
+          </Select>
+        </Stack>
+      </div>
 
-render() {
-    return (
-        <div>this is car rental Component</div>
-        )
-    }
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Stack spacing={0}>
+            <InputLabel>Pick-UP Date</InputLabel>
+            <TextField
+              id="datePick-UP-Date"
+              type="date"
+              sx={{ width: 250 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Stack direction="row" spacing={2}></Stack>
+        <Stack spacing={0}>
+          <InputLabel>Drop-Off Date</InputLabel>
+          <TextField
+            id="dateDrop-Off-Date"
+            type="date"
+            defaultValue=""
+            sx={{ width: 250 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Stack>
+      </div>
+
+      <div>
+        <Button variant="contained">Search Car</Button>
+      </div>
+    </Stack>
+  );
 }
--- src/components/travel/hotels.js
+++ src/components/travel/hotels.js
@@ -1,11 +1,103 @@
-import React, { Component } from 'react'
-import './hotels.css'
+import React, { Component } from "react";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import InputLabel from "@mui/material/InputLabel";
+import "./hotels.css";
 
-export default class HotelsComponent extends Component { 
-
-render() {
+export default class HotelsComponent extends Component {
+  render() {
     return (
-        <div>this is hotel Component</div>
-        )
-    }
+      <Stack spacing={2}>
+        <div>
+          <Stack direction="row" spacing={2}>
+            <Stack spacing={0}>
+              <InputLabel>City</InputLabel>
+              <Select
+                id="selectCity"
+                onChange=""
+                sx={{
+                  minWidth: "500px",
+                  height: "40px",
+                }}
+              />
+            </Stack>
+          </Stack>
+        </div>
+        <div>
+          <Stack direction="row" spacing={2}>
+            <Stack spacing={0}>
+              <InputLabel>Check-In</InputLabel>
+              <TextField
+                id="Check-In"
+                type="date"
+                sx={{ width: 250 }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Stack>
+            <Stack spacing={0}>
+              <InputLabel>Check-Out</InputLabel>
+              <TextField
+                id="dateCheck-Out"
+                type="date"
+                defaultValue=""
+                sx={{ width: 250 }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Stack>
+          </Stack>
+        </div>
+        <div>
+          <Stack direction="row" spacing={2}>
+            <Stack spacing={0}>
+              <InputLabel>Rooms</InputLabel>
+              <Select
+                labelId="selectRoomsClass"
+                id="demo-simple-select"
+                onChange=""
+                sx={{
+                  maxWidth: "162px",
+                }}
+              >
+                <MenuItem value={10}>1</MenuItem>
+                <MenuItem value={20}>2</MenuItem>
+                <MenuItem value={30}>3</MenuItem>
+              </Select>
+            </Stack>
+            <Stack spacing={0}>
+              <InputLabel>Adult(s)</InputLabel>
+              <TextField
+                required
+                id="textAdult"
+                type="number"
+                sx={{
+                  maxWidth: "162px",
+                }}
+              />
+            </Stack>
+            <Stack spacing={0}>
+              <InputLabel>Child</InputLabel>
+              <TextField
+                required
+                id="textChild"
+                type="number"
+                sx={{
+                  maxWidth: "162px",
+                }}
+              />
+            </Stack>
+          </Stack>
+        </div>
+        <div>
+          <Button variant="contained">Search Hotel</Button>
+        </div>
+      </Stack>
+    );
+  }
 }
