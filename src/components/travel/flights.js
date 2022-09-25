9ec18afc4f7aa7636eabb134c281979db0126b9a
LAPTOP-RD6E64U9\hp <khan.abdul.firoz@gmail.com>
2022-09-25 18:11:12 +0400

    .

--- src/components/travel/flights.js
+++ src/components/travel/flights.js
@@ -4,155 +4,161 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import InputLabel from '@mui/material/InputLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from "@mui/material/InputLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
-export default class FlightComponent extends Component {
-  render() {
-    return (
-      <Stack spacing={1}>
-        <div>
-          <RadioGroup
-            aria-labelledby="travel type"
-            defaultValue="Return"
-            name="radioTravelType"
-            row
-          >
-            <FormControlLabel value="One Way" control={<Radio />} label="One Way" />
-            <FormControlLabel value="Return" control={<Radio />} label="Return" />
-            <FormControlLabel value="Multicity" control={<Radio />} label="Multicity" />
-          </RadioGroup>
-        </div>
-        <div>
-          <Stack direction="row" spacing={2}>
-          <Stack spacing={0}>
-          <InputLabel>From</InputLabel>
-          <Select
-            id="selectFrom"
-            onChange=""
-            sx={{
-              minWidth: "250px",
-              height: "40px",
-          }}
+export default function FlightComponent() {
+  return (
+    <Stack spacing={1}>
+      <div>
+        <RadioGroup
+          aria-labelledby="travel type"
+          defaultValue="Return"
+          name="radioTravelType"
+          row
+        >
+          <FormControlLabel
+            value="One Way"
+            control={<Radio />}
+            label="One Way"
           />
-          </Stack>
-          <Stack spacing={0}>
-          <InputLabel>To</InputLabel>
-          <Select
-            id="selectTo"
-            onChange=""
-            sx={{
-            minWidth: "250px",
-            height: "40px",
-          }}
+          <FormControlLabel value="Return" control={<Radio />} label="Return" />
+          <FormControlLabel
+            value="Multicity"
+            control={<Radio />}
+            label="Multicity"
           />
-          </Stack>
-          </Stack>
-        </div>
-        <div>
-          <Stack direction="row" spacing={2}>
+        </RadioGroup>
+      </div>
+      <div>
+        <Stack direction="row" spacing={2}>
           <Stack spacing={0}>
-          <InputLabel>Departure</InputLabel>
-          <TextField
-            id="dateDeparture"
-            type="date"
-            sx={{ width: 250 }}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            />
-            </Stack>
-          <Stack spacing={0}>
-          <InputLabel>Arrival</InputLabel>
-          <TextField
-            id="dateArrival"
-            type="date"
-            defaultValue=""
-            sx={{ width: 250 }}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            />
-            </Stack>
-            </Stack>
-        </div>
-        <div>
-          <Stack direction="row" spacing={2}>
-           <Stack spacing={0}>
-          <InputLabel>Adult(s)</InputLabel>
-          <TextField
-            required
-            id="textAdult"
-            type="number"
-            sx={{
-             maxWidth: "162px"
-          }}
-            />
-            </Stack>
-          <Stack spacing={0}>
-          <InputLabel>Child</InputLabel>
-          <TextField
-            required
-            id="textChild"
-            type="number"
-            sx={{
-             maxWidth: "162px"
-          }}
-          />
-          </Stack>
-          <Stack spacing={0}>
-          <InputLabel>Infant below 2 years</InputLabel>
-          <TextField
-            required
-            id="textInfant"
-            type="number"
-            sx={{
-            maxWidth: "162px"
-          }}
-            />
-            </Stack>
-          </Stack>
-        </div>
-        <div>
-          <Stack direction="row" spacing={2}>
-            <Stack spacing={0}>
-          <InputLabel>Travel Class</InputLabel>
+            <InputLabel>From</InputLabel>
             <Select
-            labelId="selectTravelClass"
-            id="demo-simple-select"
-            onChange=""
-            sx={{
-            minWidth: "250px",
-            height: "40px",
-          }}
-          >
-            <MenuItem value={10}>Economy</MenuItem>
-            <MenuItem value={20}>Business</MenuItem>
-            <MenuItem value={30}>First</MenuItem>
-            </Select>
-            </Stack>
-          <Stack spacing={0}>
-          <InputLabel>Airlines Preference</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="selectAirlinesPref"
-            onChange=""
-            sx={{
-            minWidth: "250px",
-            height: "40px",
-          }}
-          >
-            <MenuItem value={10}>All Airlines</MenuItem>
-          </Select>
+              id="selectFrom"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            />
           </Stack>
-      </Stack>
-        </div>
-        <div>
-            <Button variant="contained">Buy Now</Button>
-        </div>
-      </Stack>
-    );
-  }
+          <Stack spacing={0}>
+            <InputLabel>To</InputLabel>
+            <Select
+              id="selectTo"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            />
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Stack spacing={0}>
+            <InputLabel>Departure</InputLabel>
+            <TextField
+              id="dateDeparture"
+              type="date"
+              sx={{ width: 250 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Stack>
+          <Stack spacing={0}>
+            <InputLabel>Arrival</InputLabel>
+            <TextField
+              id="dateArrival"
+              type="date"
+              defaultValue=""
+              sx={{ width: 250 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Stack spacing={0}>
+            <InputLabel>Adult(s)</InputLabel>
+            <TextField
+              required
+              id="textAdult"
+              type="number"
+              sx={{
+                maxWidth: "162px",
+              }}
+            />
+          </Stack>
+          <Stack spacing={0}>
+            <InputLabel>Child</InputLabel>
+            <TextField
+              required
+              id="textChild"
+              type="number"
+              sx={{
+                maxWidth: "162px",
+              }}
+            />
+          </Stack>
+          <Stack spacing={0}>
+            <InputLabel>Infant below 2 years</InputLabel>
+            <TextField
+              required
+              id="textInfant"
+              type="number"
+              sx={{
+                maxWidth: "162px",
+              }}
+            />
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Stack spacing={0}>
+            <InputLabel>Travel Class</InputLabel>
+            <Select
+              labelId="selectTravelClass"
+              id="demo-simple-select"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            >
+              <MenuItem value={10}>Economy</MenuItem>
+              <MenuItem value={20}>Business</MenuItem>
+              <MenuItem value={30}>First</MenuItem>
+            </Select>
+          </Stack>
+          <Stack spacing={0}>
+            <InputLabel>Airlines Preference</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="selectAirlinesPref"
+              onChange=""
+              sx={{
+                minWidth: "250px",
+                height: "40px",
+              }}
+            >
+              <MenuItem value={10}>All Airlines</MenuItem>
+            </Select>
+          </Stack>
+        </Stack>
+      </div>
+      <div>
+        <Button variant="contained">Buy Now</Button>
+      </div>
+    </Stack>
+  );
 }
